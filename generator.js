ada5a394762c51bce26ebb1fcfc15b147941ba74
Novik71 <vpearcesmith@gmail.com>
2018-08-22 18:47:02 +0100

    npm install and git init working

--- generator.js
+++ generator.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const userRootDir = process.cwd();
-const { spawn } = require('child_process');
+const { exec, spawn } = require('child_process');
 const ls = spawn('ls', ['-lh', '/usr']);
 
 fs.readdir('./templates', 'utf8', (err, data) => { 
@@ -38,11 +38,38 @@ fs.readdir('./templates', 'utf8', (err, data) => {
                                     fs.readdir(`${templatePath}/${item}`, (err, deepContents) => {
                                         deepContents.forEach(deepItem => {
                                             const sourceFilePathDeep = `${templatePath}/${deepItem}`;
-                                            console.log(sourceFilePathDeep);
-                                            fs.readFile(sourceFilePathDeep, 'utf8', (err, file) => {
-                                                const writePath = `${destinationPath}/${deepItem}`;
-                                                fs.writeFile(writePath, file, 'utf8', (err, file) => {
-                                                    return file;
+                                            fs.readFile(sourceFilePathDeep, 'utf8', (err, deepFile) => {
+                                                const writePathDeep = `${destinationPath}/${item}/${deepItem}`;
+                                                fs.writeFile(writePathDeep, `${deepFile}`, 'utf8', (err, file) => {
+                                                    console.log('Project folder created');
+                                                    console.log('Installing npm modules...');
+                                                    exec('npm install', { cwd : destinationPath, encoding : 'utf8' }, (err, stdout, stderr) => {
+                                                        console.log('npm package successfully installed');
+                                                        console.log('Installing test suite...')
+                                                        exec('npm install chai', { cwd : destinationPath, encoding : 'utf8' }, (err, stdout, stderr) => {
+                                                            exec('npm install mocha', { cwd : destinationPath, encoding : 'utf8' }, (err, stdout, stderr) => {
+                                                                console.log('Test suite successfully installed'); 
+                                                                inquirer.prompt({
+                                                                    name : 'createGit',
+                                                                    message : 'Do you want to create a git repo?',
+                                                                    type : 'confirm'                                                                                                                                    
+                                                                    }).then(answers => {
+                                                                        if (answers.createGit === true) {
+                                                                            exec('git init ; git add ./ ; git commit -m "first commit"', { cwd : destinationPath, encoding : 'utf8' }, (err, stdout,stderr) => {
+                                                                                console.log('Git repo created, with initial commit');
+                                                                                exec('code ./index.js', { cwd : destinationPath, encoding : 'utf8' }, (err, stdout, stderr) => {
+                                                                                    console.log('Project Setup Complete!');
+                                                                                });
+                                                                            })
+                                                                        } else {
+                                                                            exec('code .', { cwd : destinationPath, encoding : 'utf8' }, (err, stdout, stderr) => {
+                                                                                console.log('Project Setup Complete!');
+                                                                        });
+                                                                    }
+                                                                });                                                             
+                                                            });
+                                                        });
+                                                    });
                                                 });                                
                                             });                                      
                                         });
